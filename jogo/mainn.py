5f27131d2cb8c23d11ac96711ef55c865846a359
memele <50452427+memelee0@users.noreply.github.com>
2020-03-12 08:53:57 -0300

    Update mainn.py

--- jogo/mainn.py
+++ jogo/mainn.py
@@ -1,4 +1,4 @@
-import pygame, random, sys
+import pygame, random, sys, os
 from pygame import *
 
 pygame.init()
@@ -14,6 +14,24 @@ class MainWindow:
         self.display.blit(img, (0,0))            
 mainWindow = MainWindow(800,600)
 
+class square:
+    def __init__(self, width, height):
+        self.width=40
+        self.height=60
+        self.x=40
+        self.y=34
+        self.tela=(800,600)
+        self.sq=pygame.draw.rect(self.tela, (255, 0, 0), (self.x, self.y, self.width, self.height))
+    def back_square(self):
+        self.display.blit(self.sq)
+#x = 50
+#y = 50
+#width = 40
+#height = 60
+#vel = 5
+#win = ((800,600))
+#def quadradro():
+#    pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))
 
 while True:
     for event in pygame.event.get():
@@ -21,6 +39,6 @@ while True:
             pygame.exit()
             sys.exit()
         mainWindow.background()
-    
-        
+        square.back_square()
+
     pygame.display.update()
